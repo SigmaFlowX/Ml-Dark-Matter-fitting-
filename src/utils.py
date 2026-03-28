4eba04c7ba43884271eadba12a53b22506304b01
Dmitrii <bakaevdima4@gmail.com>
2026-03-28 14:21:06 +0300

    minor changes

--- src/utils.py
+++ src/utils.py
@@ -11,5 +11,5 @@ def nu_plummer_profile(r, a, m0):
     return 3 * m0 / (4 * 3.14 * a**3) * (1 + r**2 / a**2)**(-5/2)
 
 def mass_nfw(r, rho_s, r_s): # M(<r)
-    integral = lambda x: 4 *  np.pi * x**2 * nfw_profile(r, rho_s, r_s)
+    integral = lambda x: 4 *  np.pi * x**2 * rho_nfw(r, rho_s, r_s)
     return quad(integral, 0, r)
