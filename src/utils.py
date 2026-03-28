678998f80b52effef1c4e71fbea0f19ce006393c
Dmitrii <bakaevdima4@gmail.com>
2026-03-28 14:04:21 +0300

    added plummer profile

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,8 @@
-
-
 def nsfw_profile(rho_s, r_s, r):
     return rho_s/(r/r_s)/(1+r/r_s)**2
 
 def osmirnov_anisotropy(beta_inf, r_beta, r):
     return beta_inf * r / (r + r_beta)
+
+def plummer_profile(a, m0, r):
+    return 3 * m0 / (4 * 3.14 * a**3) * (1 + r**2 / a**2)**(-5/2)
