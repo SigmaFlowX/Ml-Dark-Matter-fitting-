db6315406f975065161ec2f1af6a67fe733981a0
Dmitrii <bakaevdima4@gmail.com>
2026-03-30 11:10:23 +0300

    added sigma2 interpolation

--- src/create_synthetic_data.py
+++ src/create_synthetic_data.py
@@ -1,12 +1,16 @@
 from solve_jeans_eq import solve_jeans_eq
 import numpy as np
-import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
+
 
 def create_data(r_grid, rho_s, r_s, a, m0, beta_inf, r_beta, n_stars):
     sigma2 = solve_jeans_eq(r_grid, rho_s, r_s, a, m0, beta_inf, r_beta)
+    sigma2_funct =  interp1d(r_grid, sigma2, kind="cubic", fill_value="extrapolate")
 
     radii = generate_star_radii_analytic(n_stars, a)
-     #.....
+
+    print(radii)
+
 
 def generate_star_radii_analytic(n_stars, a):
     u = np.random.uniform(0, 1, n_stars)
@@ -14,10 +18,17 @@ def generate_star_radii_analytic(n_stars, a):
     return r_samples
 
 if __name__ == "__main__":
-    samples = generate_star_radii_analytic(5000, 1)
 
-    plt.hist(samples, bins=100, density=True)
-    plt.show()
+    create_data(
+        r_grid = np.logspace(-2, 1, 120),
+        rho_s = 0.1,
+        r_s = 5,
+        a = 1,
+        m0 = 1e7,
+        beta_inf = 0.3,
+        r_beta = 2,
+        n_stars = 10
+    )
 
 
 
